4fc446390dc0344806a746353b01a6ebd0cd4385
Dmitry <sharonov-db@yandex.ru>
2023-12-03 16:04:43 +0400

    Commit_031223_3

---
+++ static/admin/Conf/bash_script.sh
@@ -0,0 +1,9 @@
+export USER_DB=dbs
+export PASSWORD_DB=1
+export SECRET_KEY='django-insecure-h^4xe($$#197_m%$0#zx2$39yn&jz%%=%s04*@-&rxtv>
+export REDIS1=redis://defalt:1@127.0.0.1:6379/0
+export REDIS2=redis://defalt:1@127.0.0.1:6379/1
+
+export EMAIL_HOST_USER=sharonovdb@yandex.ru
+export EMAIL_HOST_PASSWORD=yrpqbdciqacolihy
+
---
+++ static/admin/Conf/gunicorn_conf.py
@@ -0,0 +1,4 @@
+command = '/home/diman/venv/bin/gunicorn'
+pythonpath = '/home/diman/LMS5_Project'
+bind = '195.58.51.97:8000'
+workers = 5
